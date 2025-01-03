038fd3902eb5310c98ec0d4c634a1ae7ceb6ffdc
Sahil Ghanwat <Sahilghanwatdev@gmail.com>
2025-01-03 20:41:01 +0530

    Changes made in routes.server

--- Project-Front-end/frontend/src/app/app.routes.server.ts
+++ Project-Front-end/frontend/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/task/:id/edit',
+    renderMode: RenderMode.Client, // Renders on the client side
+  },
+  {
+    path: 'employee/task/:id/view',
+    renderMode: RenderMode.Client, // Renders on the client side
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
